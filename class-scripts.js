f04673d335247b1df164ca7053236c3b04d611a3
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-02-22 14:29:34 +0430

    Remove the linters error.

--- class-scripts.js
+++ class-scripts.js
@@ -1,55 +1,19 @@
 // Book Class: Represents a Book
+// eslint-disable-next-line max-classes-per-file
 class Book {
-    constructor(title, author, isbn) {
-      this.title = title;
-      this.author = author;
-      this.isbn = isbn;
-    }
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
-
-// UI Class: Handle UI Tasks
-
-class UI {
-    static displayBooks(){
-        const books = Store.getBooks();
-
-        books.forEach((book) => UI.addBookToList(book));
-    }
-
-    static addBookToList(book){
-      const list = document.querySelector('#book-list');
-
-      const row = document.createElement('tr');
-
-      row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>
-      `;
-
-      list.appendChild(row);
-    }
-
-    static deleteBook(el) {
-      if(el.classList.contains('delete')){
-        el.parentElement.parentElement.remove();
-      }
-    }
-
-    static clearFields() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
-      document.querySelector('#isbn').value = '';
-    }
 }
 
 // Store Class: Handle Storage
 
 class Store {
-  static getBooks(){
+  static getBooks() {
     let books;
-    if (localStorage.getItem('books') === null){
+    if (localStorage.getItem('books') === null) {
       books = [];
     } else {
       books = JSON.parse(localStorage.getItem('books'));
@@ -58,22 +22,59 @@ class Store {
     return books;
   }
 
-  static addBook(book){
+  static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static removeBook(isbn){
+  static removeBook(isbn) {
     const books = Store.getBooks();
 
     books.forEach((book, index) => {
-      if (book.isbn === isbn){
+      if (book.isbn === isbn) {
         books.splice(index, 1);
       }
     });
-    
+
     localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>
+      `;
+
+    list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
   }
 }
 
@@ -82,7 +83,6 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-
   // prevent default submit
   e.preventDefault();
 
@@ -92,7 +92,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const isbn = document.querySelector('#isbn').value;
 
   // valitdate empty form
-  if (title === '' || author === '' || isbn === ''){
+  if (title === '' || author === '' || isbn === '') {
     alert('Please fill in all fields!');
   } else {
     // making object of Book class
@@ -107,12 +107,10 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     // clear fields
     UI.clearFields();
   }
-
 });
 
 // Event: Delete a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-
   // remove book from UI
   UI.deleteBook(e.target);
 
