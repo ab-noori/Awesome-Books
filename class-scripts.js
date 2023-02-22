60892f25e3132e858cfbe9390bd13413110d679a
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-02-22 11:22:35 +0430

    Add and Remove Data to and from localStorage.

--- class-scripts.js
+++ class-scripts.js
@@ -11,30 +11,7 @@ class Book {
 
 class UI {
     static displayBooks(){
-        const storedBooks = [
-            {
-              title: 'Book one',
-              author: 'Abdul Ali Noori',
-              isbn: '313313'
-            },
-            {
-              title: 'Book two',
-              author: 'Abdul Ali Noori',
-              isbn: '313312'
-            },
-            {
-              title: 'Book two',
-              author: 'Abdul Ali Noori',
-              isbn: '313312'
-            },
-            {
-              title: 'Book two',
-              author: 'Abdul Ali Noori',
-              isbn: '313312'
-            },
-        ];
-
-        const books = storedBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -69,6 +46,37 @@ class UI {
 
 // Store Class: Handle Storage
 
+class Store {
+  static getBooks(){
+    let books;
+    if (localStorage.getItem('books') === null){
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book){
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn){
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn){
+        books.splice(index, 1);
+      }
+    });
+    
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
@@ -83,17 +91,31 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
-  // making object of Book class
-  const book = new Book(title, author, isbn);
+  // valitdate empty form
+  if (title === '' || author === '' || isbn === ''){
+    alert('Please fill in all fields!');
+  } else {
+    // making object of Book class
+    const book = new Book(title, author, isbn);
 
-  // add book to UI
-  UI.addBookToList(book);
+    // add book to UI
+    UI.addBookToList(book);
 
-  // clear fields
-  UI.clearFields();
+    // add book to store
+    Store.addBook(book);
+
+    // clear fields
+    UI.clearFields();
+  }
+
 });
 
 // Event: Delete a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
+
+  // remove book from UI
   UI.deleteBook(e.target);
+
+  // remove book from Store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
