f6848002650d21a65722aa492096813d6c2da935
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-02-22 09:47:18 +0430

    Apply add a new book feature.

--- class-scripts.js
+++ class-scripts.js
@@ -22,6 +22,16 @@ class UI {
               author: 'Abdul Ali Noori',
               isbn: '313312'
             },
+            {
+              title: 'Book two',
+              author: 'Abdul Ali Noori',
+              isbn: '313312'
+            },
+            {
+              title: 'Book two',
+              author: 'Abdul Ali Noori',
+              isbn: '313312'
+            },
         ];
 
         const books = storedBooks;
@@ -30,6 +40,49 @@ class UI {
     }
 
     static addBookToList(book){
+      const list = document.querySelector('#book-list');
 
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>
+      `;
+
+      list.appendChild(row);
+    }
+
+    static clearFields(){
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
+      document.querySelector('#isbn').value = '';
     }
 }
+
+// Store Class: Handle Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+  // prevent default submit
+  e.preventDefault();
+
+  // get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // making object of Book class
+  const book = new Book(title, author, isbn);
+
+  // add book to UI
+  UI.addBookToList(book);
+
+  // clear fields
+  UI.clearFields();
+})
